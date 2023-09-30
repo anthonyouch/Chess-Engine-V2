08e0986eecf1f3ed8fcfa0e153870b103e2dfbba
anthonyouch <anthonyouch.programmer@gmail.com>
2023-09-30 16:19:52 +1000

    Added Lichess API
    bot connected to lichess api and is able to accept challenges.

---
+++ lichess.py
@@ -0,0 +1,51 @@
+import requests
+import json
+
+class LichessAPI:
+
+    def __init__(self):
+        self.token = "lip_drFmC9zrFuqtzyNdvs77"
+        self.session = requests.Session()
+
+        self.header = {
+            "Authorization": f"Bearer {self.token}"
+        }
+
+        self.session.headers.update(self.header)
+
+    def get_event(self):
+        """Send a GET Request to lichess.org to get event
+        Each non-empty line is a JSON object containing a type field. Possible values are:
+        gameStart Start of a game
+        gameFinish Completion of a game
+        challenge A player sends you a challenge or you challenge someone
+        challengeCanceled A player cancels their challenge to you
+        challengeDeclined The opponent declines your challenge
+        """
+        url = "https://lichess.org/api/stream/event"
+
+        response = self.session.get(url, stream=True)
+        return response
+
+    def accept_challenge(self, challengeId):
+        url = f"https://lichess.org/api/challenge/{challengeId}/accept"
+        self.session.post(url)
+
+    def make_move(self, gameId, move):
+        """https://lichess.org/api/bot/game/{gameId}/move/{move}"""
+        url = f"https://lichess.org/api/bot/game/{gameId}/move/{move}"
+        self.session.post(url)
+
+while True:
+    lichessAPI = LichessAPI()
+    response = lichessAPI.get_event()
+    # if response == "challenge":
+
+    for line in response.iter_lines():
+
+        if json.loads(line)['type'] == "challenge":
+            print(json.loads(line)['challenge']['id'])
+            lichessAPI.accept_challenge(json.loads(line)['challenge']['id'])
+            
+
+
